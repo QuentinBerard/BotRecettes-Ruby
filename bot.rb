77156fcc9e4bd6c599fb5be2826fd2b4cc205477
root <root@LAPTOP-SAER174R.localdomain>
2020-11-17 17:12:22 +0100

    oui d'accord très bien

--- bot.rb
+++ bot.rb
@@ -1,34 +1,28 @@
 require 'json'
-
-
 json = File.read('recipes.json')
 object = JSON.parse(json)
-
 puts "Comment tu t'appelles ?"
 name = gets.chomp
 puts "Salut a toi #{name}"
 count = 0
 #puts object[1]["recipe_name"]
-
-def dans_ton_frigo()
+while count == 0 do
   puts "Quels sont tes ingredients, #{name} ?"
   ingredient_frigo = gets.chomp.capitalize
-  puts ingredient_frigo
   object.each do |recette|
     if recette["ingredients"].include?ingredient_frigo
       count += 1
-      puts "Et si on cuisinait un "+recette["recipe_name"]+" ?"
+      if count==1
+        puts "Et si on cuisinait un "+recette["recipe_name"]+" ?"
+      end
+      if count>1
+        puts "ou alors un "+recette["recipe_name"]+" ?"
+        puts "Quelle recette tu préfères ?"
+        #reponse_recette = gets.chomp.capitalize
+      end
     end
   end
+  if count == 0
+      puts "Déso, essayes un autre ingredient mon ptit pote !"
+  end
 end
-
-if count == 0
-  puts "Déso, essayes un autre ingredient mon ptit pote !"
-  dans_ton_frigo()
-end
-
-# if object[0]["ingredients"].include?ingredient_frigo
-#     puts "Et si on cuisinait un "+object[0]["recipe_name"]+"?"
-#   else
-#     puts "quoi ?"
-# end

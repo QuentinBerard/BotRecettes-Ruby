91799bba5efd916f086aec930532c1949b8fc94e
manon-coulombe <manon.coulombe@student.adatechschool.fr>
2020-11-17 18:47:41 +0100

    essaie brance

--- bot.rb
+++ bot.rb
@@ -14,17 +14,20 @@ while count == 0 do
   puts "Quels sont tes ingredients, #{name} ?"
   ingredient_frigo = gets.chomp.capitalize
   object.each do |recette|
-    if recette["ingredients"].include?ingredient_frigo
-      count += 1
-      if count==1
-        puts "Et si on cuisinait un "+recette["recipe_name"]+" ?"
-      end
-      if count>1
-        puts "ou alors un "+recette["recipe_name"]+" ?"
+    recette["ingredients"].each do |ingredient_recette|
+    #  puts ingredient_recette
+      if ingredient_frigo.include?ingredient_recette
+        count += 1
+        if count==1
+          puts "Et si on cuisinait un "+recette["recipe_name"]+" ?"
+        end
+        if count>1
+          puts "ou alors un "+recette["recipe_name"]+" ?"
+        end
       end
     end
-  end
-  if count == 0
+    if count == 0
       puts "Déso, essayes un autre ingredient mon ptit pote !"
+    end
   end
 end

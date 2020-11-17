0129d7fb57752ce814237fcbecc4b9ff9ceafc8c
Margaux Lenne <margaux.lennebonety@gmail.com>
2020-11-17 15:57:54 +0100

    Branche Margaux added

--- bot.rb
+++ bot.rb
@@ -1,3 +1,14 @@
-puts "Comment tu t'appelles ?"
+require 'json'
+
+=begin puts "Comment tu t'appelles ?"
 name = gets.chomp
 puts "Salut a toi #{name}"
+=end
+
+json = File.read('recipes.json')
+object = JSON.parse(json)
+puts object[1]["recipe_id"]
+
+
+
+	

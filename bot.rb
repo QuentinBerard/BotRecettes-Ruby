bb50ef2fce024da2377575fa945f5e074f718b67
manon-coulombe <manon.coulombe@student.adatechschool.fr>
2020-11-18 17:38:30 +0100

    jour3

--- bot.rb
+++ bot.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'pp'
 
 json = File.read('recipes.json')
 object = JSON.parse(json)
@@ -26,5 +27,17 @@ while count == 0 do
   end
   if count == 0
       puts "Déso, essayes un autre ingredient mon ptit pote !"
+  end
+end
+
+puts "Alors #{name}, que souhaites-tu cuisiner ?"
+choix = gets.chomp.capitalize
+object.each do |recette|
+  if recette["recipe_name"].include?choix
+    puts "C'est parti ! Tu auras besoin de :"
+    recette["ingredients"].each do |quantite|
+      puts quantite.to_s
+    end
+    #print recette["ingredients"].split(",")
   end
 end
